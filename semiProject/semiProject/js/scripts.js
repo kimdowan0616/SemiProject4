fb5ad5bf2d89d706526deee636c74278f4d25666
kimdowan0616 <ourbaby7@nate.com>
2021-12-07 16:43:42 +0900

    commit dw

---
+++ semiProject/semiProject/js/scripts.js
@@ -0,0 +1,78 @@
+/*!
+* Start Bootstrap - Freelancer v7.0.5 (https://startbootstrap.com/theme/freelancer)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 72,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+	
+
+
+	const slickSlide = jQuery('#slick-slide')
+	
+	if(slickSlide){
+		slickSlide.slick({
+			dots: true,
+			arrows: false,
+			slidesToShow:4,
+			slideToScroll:1,
+			autoplay:true,
+			autoplaySpeed:2000,
+			responsive:[
+				{
+					breakpoint: 768,
+					settings:{slidesToShow:2}
+				},
+				{
+					breakpoint:576,
+					settings:{slidesToShow:1}
+				}
+			]
+		})
+	}
+
+});
